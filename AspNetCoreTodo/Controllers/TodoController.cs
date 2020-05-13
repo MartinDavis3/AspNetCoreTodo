9ef345a961c551a155a7dd9f0fc5ad9c06984071
Martin Davis <martin_davis3@yahoo.ca>
2020-05-13 15:04:13 -0600

    initial commit

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        //ACTION https://localhost:5000/Todo  - since the url has Todo at the end, program knows to use the TodoController.
+        public IActionResult Index()
+        {
+            // Get to-do items from database
+            // Put items into a model
+            // Render view using the model
+        }
+
+    }
+}
+
---
+++ AspNetCoreTodo/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreTodo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }     //GUID is just an automatically generated string as an identifier
+        public bool IsDone { get; set; }
+        [Required]       //Specifies that next item (Title) is required (can't be missing)
+        public string Title { get; set; }
+        public DateTimeOffset? DueAt { get; set; }   //? means that DateTimeOffset can explicitly be set to NULL
+    }
+}
+
