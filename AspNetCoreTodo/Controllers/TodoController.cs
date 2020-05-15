29d9528b90dc7073c00982391e330239f02f6216
Martin Davis <martin_davis3@yahoo.ca>
2020-05-14 23:36:37 -0600

    first working solution

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo.Models;
 using Microsoft.AspNetCore.Mvc;
+
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        //ACTIONS: These are routes handled by controllers. Here https://localhost:5000/Todo  - since the url has Todo at the end, program knows to use the TodoController.
-        public IActionResult Index()
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)   //This is a constructor
         {
+            _todoItemService = todoItemService;
+        }
+
+
+        //ACTIONS: These are routes handled by controllers. Here https://localhost:5000/Todo  - since the url has Todo at the end, program knows to use the TodoController.
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
             // Get to-do items from database
             // Put items into a model
             // Render view using the model
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+            return View(model);
         }
 
     }
